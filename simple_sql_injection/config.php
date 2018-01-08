4c45aab4f9669e2607ddb889d9c2c248a75e2cd8
HardPlant <abc7988se@naver.com>
2018-01-08 15:20:02 +0900

    merge completed

--- simple_sql_injection/config.php
+++ simple_sql_injection/config.php
@@ -6,8 +6,5 @@ $mysql_database = "wordpress";
 
 $conn = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password,$mysql_database) or die("db connect error");
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 436481396fb0c62a74a44542af15a6cd6f7290cd
 ?>
